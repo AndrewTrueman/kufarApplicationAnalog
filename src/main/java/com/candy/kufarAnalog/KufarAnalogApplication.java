3dd682db976cf72c39e03e3f33bfb994f019deea
candy <andrewmail2001@gmail.com>
2024-07-13 11:18:54 +0300

    branches magic

---
+++ src/main/java/com/candy/kufarAnalog/KufarAnalogApplication.java
@@ -0,0 +1,13 @@
+package com.candy.kufarAnalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KufarAnalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KufarAnalogApplication.class, args);
+	}
+
+}
