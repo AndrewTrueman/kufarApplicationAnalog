2350a73cb0878505ca625efb813d71b8c308603e
candy <andrewmail2001@gmail.com>
2024-07-14 19:49:02 +0300

    Simple product controller logic added, product list display and CRUD operations, data stored in ArrayList temporary

--- src/main/java/com/candy/kufarAnalog/KufarAnalogApplication.java
+++ src/main/java/com/candy/kufarAnalog/KufarAnalogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KufarAnalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KufarAnalogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KufarAnalogApplication.class, args);
+    }
 
 }
